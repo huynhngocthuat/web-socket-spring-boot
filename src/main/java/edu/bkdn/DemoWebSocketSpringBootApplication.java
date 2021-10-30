dc20bfa429922bab311049f1c6308ad93dbd5866
= <=>
2021-10-30 22:01:52 +0700

    first commit

---
+++ src/main/java/edu/bkdn/DemoWebSocketSpringBootApplication.java
@@ -0,0 +1,13 @@
+package edu.bkdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebSocketSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebSocketSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bkdn/DemoWebSocketSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bkdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebSocketSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
